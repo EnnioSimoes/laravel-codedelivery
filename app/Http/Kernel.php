b6f91d2813815f88b235b7f6ae0191060ef3a7c1
Ennio Simões <ennio.simoes@outlook.com>
2015-11-12 21:50:35 -0200

    Criado middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -34,6 +34,8 @@ class Kernel extends HttpKernel
         'guest' => \CodeDelivery\Http\Middleware\RedirectIfAuthenticated::class,
         'auth.checkrole' => \CodeDelivery\Http\Middleware\CheckRole::class,
 
+        'oauth.checkrole' => \CodeDelivery\Http\Middleware\CheckRole::class,
+
         //Para autenticacao Oauth
         'oauth' => \LucaDegasperi\OAuth2Server\Middleware\OAuthMiddleware::class,
         'oauth-user' => \LucaDegasperi\OAuth2Server\Middleware\OAuthUserOwnerMiddleware::class,
---
+++ app/Http/Middleware/OAuthCheckRole.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace CodeDelivery\Http\Middleware;
+
+use Closure;
+use CodeDelivery\Repositories\UserRepository;
+use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+
+class OAuthCheckRole
+{
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $role)
+    {
+        $id = Authorizer::getResourceOwnerId();
+        $user = $this->userRepository->fine($id);
+
+        if($user->role != $role) {
+            abort(403, 'Access Forbiddem');
+        }
+
+        return $next($request);
+    }
+}
